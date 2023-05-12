80ff3d33a03fab7e7394efa0e898556514b76244
Suman Chapai <sumanchapai@gmail.com>
2023-05-12 00:33:31 -0500

    Update redis key env file

--- python/test_redis_reader.py
+++ python/test_redis_reader.py
@@ -7,7 +7,7 @@ import redis
 r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
 
 # Also read the .env configuration once
-config = dotenv_values(".env")
+config = dotenv_values("../.env")
 redis_env_temp_key = "TEMP_KEY"
 
 while True:
